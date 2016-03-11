73631459e50b0fbb4e7261bccff4888f738d2cf6
Akmp40 <Akmp40@gmail.com>
2016-03-11 10:47:02 +0100

    Update helperfunctions.h
    Added LimitStop.

--- helperfunctions.h
+++ helperfunctions.h
@@ -2,4 +2,6 @@
 #ifndef HELPERFUNCTIONS_H_INCLUDED 
 #define HELPERFUNCTIONS_H_INCLUDED
 
-int LimitModulo(int limit, int direction, int value);
+int LimitModulo(int limit, int direction, int value);// Starts from 0 after reaching max limit
+
+int LimitStop(int upperLimit, int lowerLimit, int direction, int value); // stops when reaching max limit, or min limit
