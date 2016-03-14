f5f9648c25e10089c3d6b0a90772bb00a44117c7
Jacob Hejderup <jacobhejderup@gmail.com>
2016-03-14 08:10:36 +0100

    Update helperfunctions.c
    Changed LimitModulo so that it only returns positive numbers

--- helperfunctions.c
+++ helperfunctions.c
@@ -4,7 +4,8 @@ include "helperfunctions.h"
 int LimitModulo (int limit, int direction, int value) //Helperfunctions for puttning a limit on int
 {
 	value += direction;
-	return value = value % limit;
+	value = value % limit;
+	return value < 0 ? value + limit : value;
 }
 
 int LimitStop(int upperLimit, int lowerLimit, int direction, int value)
