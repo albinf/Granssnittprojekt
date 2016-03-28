1aa10fbe6c909575cabd6e7e67304edbbe0f9128
Jacob Hejderup <jacobhejderup@gmail.com>
2016-03-28 12:07:41 +0200

    Added files via upload

--- helperfunctions.c
+++ helperfunctions.c
@@ -1,13 +1,22 @@
-//AUTHOR: JACOB HEJDERUP
-include "helperfunctions.h"
+/*
+*	NAME: helperfunctions.c
+*	AUTHOR: Jacob Hejderup
+*	PROJECT: 
+*	DATE:
+*	CODE REVISION:
+*	PURPOSE OF FILE: A c file for helperfunctions. 
+*	The purpose of the helperfunctions module is to make the code eaiser to read
+*	and keep the code relative short.
+*/
+#include "helperfunctions.h"
 
-int LimitModulo (int limit, int direction, int value) //Helperfunctions for puttning a limit on int
+
+int LimitModulo(int limit, int direction, int value)
 {
-	value += direction;
+        value += direction;
 	value = value % limit;
 	return value < 0 ? value + limit : value;
 }
-
 int LimitStop(int upperLimit, int lowerLimit, int direction, int value)
 {
 	value += direction;
@@ -21,18 +30,36 @@ int LimitStop(int upperLimit, int lowerLimit, int direction, int value)
 	}
 	return value;
 }
-
 int ChangeOnRelease()
 {
-	if(BUTTON_SOMETHING == 1)
+    static int button_pressed = 0;
+	if(PORTAbits.RA3 == 1)
 	{
 		button_pressed = 1;
 		return 0;
 	}
-	if(BUTTON_SOMETHING == 0) && (button_pressed == 1)
+        else if((PORTAbits.RA3 == 0) && (button_pressed == 1))
 	{
 		button_pressed = 0;
-		return 1;	
+		return 1;
 	}
-	
+        else
+        {
+            return 0;
+        }
+
+}
+
+int ipow(int base, int exp)
+{
+    int result = 1;
+    while (exp)
+    {
+        if (exp & 1)
+            result *= base;
+        exp >>= 1;
+        base *= base;
+    }
+
+    return result;
 }
--- helperfunctions.h
+++ helperfunctions.h
@@ -1,9 +1,72 @@
-//AUTHOR: JACOB HEJDERUP
-#ifndef HELPERFUNCTIONS_H_INCLUDED 
-#define HELPERFUNCTIONS_H_INCLUDED
+/*
+*	NAME: helperfunctions.h
+*	AUTHOR: Jacob Hejderup
+*	PROJECT: 
+*	DATE:
+*	CODE REVISION:
+*	PURPOSE OF FILE: A header file for helperfunctions. 
+*	The purpose of the helperfunctions module is to make the code eaiser to read
+*	and keep the code relative short.
+*/
 
-int LimitModulo(int limit, int direction, int value);// Starts from 0 after reaching max limit
 
-int LimitStop(int upperLimit, int lowerLimit, int direction, int value); // stops when reaching max limit, or min limit
+#ifndef HELPERFUNCTIONS_H
+#define	HELPERFUNCTIONS_H
+#include "gui.h"
+#include <stdlib.h>
+#include <xc.h>
+#include <libpic30.h>
 
-int ChangeOnRelease();//Change something on release
+
+/***********************************************************************
+*LimitModulo
+*	Function Description: 
+*		Add or subtract 1 out of value and puts a limit on value. 
+*		when the value reaches the limit, the value resets to zero via modulo. 
+*		The direction variable decides if value should be value + 1 or value  - 1. 
+*		returns value after checking limit.
+*
+*	Input: int limit, int direction, int value
+*	Output: int value
+***********************************************************************/
+int LimitModulo(int limit, int direction, int value);
+
+/***********************************************************************
+*LimitStop
+*	Function Description: 
+*		Add or subtract 1 out of value and puts a limit on value. 
+*		when the value reaches the limit, the value gets set to lowerLimit or upperLimit 
+*		depending if value is lower than lowerLimit or higher than upperlimit. 
+*		The direction variable decides if value should be value + 1 or value  - 1. 
+*		returns value after checking limit.
+*
+*	Input: int upperLimit, int lowerLimit, int direction, int value
+*	Output: int value
+***********************************************************************/
+int LimitStop(int upperLimit, int lowerLimit, int direction, int value);
+
+/***********************************************************************
+*ChangeOnRelease
+*	Function Description: 
+*		Check if a button is pressed, if that button is pressed set button_pressed to 1 
+*		and return 0. If that button is not pressed and button_pressed is 1, 
+*		set the button_pressed to 0 and return 1. 
+*		
+*	Input: void
+*	Output: int 0 or 1
+***********************************************************************/
+int ChangeOnRelease();
+
+/***********************************************************************
+*ipow
+*	Function Description: 
+*		Calculates base power of exp.
+*		
+*	Input: int base, int exp
+*	Output: int value
+***********************************************************************/
+int ipow(int base, int exp);
+
+
+#endif	/* HELPERFUNCTIONS_H */
+
